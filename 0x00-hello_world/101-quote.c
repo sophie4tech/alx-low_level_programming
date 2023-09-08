8b25e4b492c5795335e2fa943ef335df98f21a4e
sophie4tech <sophiannau@gmail.com>
2023-09-08 15:23:06 -0700

    Understanding simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * main - this is the main entry
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+    system("echo 'and that piece of art is useful\" \
+ - Dora Korpar, 2015-10-19'");
+
+    return (1);
+}
