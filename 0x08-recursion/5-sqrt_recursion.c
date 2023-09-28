640ae41671e39209fc9d4a271467c0c26e653d73
sophie4tech <sophiannau@gmail.com>
2023-09-28 07:35:36 -0700

    Prime number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to check
+ * @i: number to check
+ *
+ * Return: Square root
+ */
+int _sqrt_recursive_helper(int n, i)
+{
+
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (_sqrt_recursion_helper(n, i + 1));
+	int _sqrt_recursive(n)
+		return (_sqrt_recursive_helper(n, 0));
+}
