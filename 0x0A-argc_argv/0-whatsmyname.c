664b8f013fbfb1cbc0e2d03e36174ef93567d317
sophie4tech <sophiannau@gmail.com>
2023-10-06 12:08:11 -0700

    My new name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argv:  arrays of size
+ * @argc: number of arguments
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int y;
+
+	for (y = 0; y < argc; y++)
+	printf("%s\n", argv[y]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef _HEAD_H
+#define _HEAD_H
+
+#include <stdio.h>
+int main(int argc, char *argv[]);
+int _putchar(char c);
+
+#endif
