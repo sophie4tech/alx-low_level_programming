5cfb1e321aa61e51bf95e67c49f632d20a21b947
sophie4tech <sophiannau@gmail.com>
2023-10-08 15:23:51 -0700

    Multiplication extended

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <stddef.h>
+#include <string.h>
 void *malloc_checked(unsigned int b);
 int _putchar(char c);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
