4603bd37f7140def7e80d220968503543607023e
sophie4tech <sophiannau@gmail.com>
2023-10-11 12:37:48 -0700

    Initialize struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,7 @@ typedef struct dog
 	char *owner;
 } dg;
 
-void int_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dg *new_dog(char *name, float age, char *owner);
 void free_dog(dg *d);
