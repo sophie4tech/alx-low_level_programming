385467d96c44371530c283842260d86173d35afb
sophie4tech <sophiannau@gmail.com>
2023-10-11 12:18:25 -0700

    Struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - function to create struct dog
+ * @name: character to print
+ * @age: number to print
+ * @owner: character to print
+ * Return: dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dg;
+
+void int_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dg *new_dog(char *name, float age, char *owner);
+void free_dog(dg *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+#endif /* DOG_H */
