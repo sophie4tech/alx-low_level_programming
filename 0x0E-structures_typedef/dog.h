a6c121a00fe1821bbb0e7aaacf77d095c1260fce
sophie4tech <sophiannau@gmail.com>
2023-10-11 12:58:34 -0700

    Change new

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,12 +13,12 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dg;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dg *new_dog(char *name, float age, char *owner);
-void free_dog(dg *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 #endif /* DOG_H */
